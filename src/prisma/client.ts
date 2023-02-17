c25fbfb8288dfdd22a6c56ab2cf13a4e5ec7e079
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2023-02-17 11:30:38 +0000

    format code

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,19 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-let db: PrismaClient;
+let db: PrismaClient
 
 declare global {
-  var __db: PrismaClient | undefined;
+  var __db: PrismaClient | undefined
 }
 
 // this is needed to stop creating a new instance of Prisma everytime the server restarts
-if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient();
+if (process.env.NODE_ENV === 'production') {
+  db = new PrismaClient()
 } else {
   if (!global.__db) {
-    global.__db = new PrismaClient();
+    global.__db = new PrismaClient()
   }
-  db = global.__db;
+  db = global.__db
 }
 
-export { db };
+export { db }
